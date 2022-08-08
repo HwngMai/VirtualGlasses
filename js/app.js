27fb859338ab5be6f60fdd72eddfec7533835997
Hung Mai <hwngmvi@gmail.com>
2022-08-08 11:21:51 +0700

    complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 import { showKinh } from "./controller.js";
 import { dataGlasses } from "./main.js";
 showKinh(dataGlasses);
+//**FUNC removeglasses */
+let removeGlasses = (isRemove) => {
+  if (isRemove) {
+    document.getElementById("virtualImg").style.display = "block";
+    document.getElementById("glassesInfo").style.display = "block";
+  } else {
+    document.getElementById("virtualImg").style.display = "none";
+    document.getElementById("glassesInfo").style.display = "none";
+  }
+};
+window.removeGlasses = removeGlasses;
--- js/controller.js
+++ js/controller.js
@@ -6,7 +6,7 @@ export let showKinh = (dsKinh) => {
     contentHTML += `
     <div class="d-flex col-4">
       <button 
-        onclick="tryKinh(this.id)"
+        onclick="tryKinh(this.id), showInfo(this.id)"
         id="${i}" value="${i}"
         class="btn">
       <img class="img-fluid" src=${dsKinh[i].src}>
@@ -19,9 +19,22 @@ export let showKinh = (dsKinh) => {
 export let tryKinh = (clickId) => {
   let contentHTML = "";
   //lấy src url đưa vào contentHTML
-  contentHTML = `<img src=${dataGlasses[clickId].virtualImg}>`;
+  contentHTML = `<img id="virtualImg" src=${dataGlasses[clickId].virtualImg}>`;
   console.log("contentHTML virtual: ", contentHTML);
   //show contentHTML
   document.getElementById("avatar").innerHTML = contentHTML;
 };
 window.tryKinh = tryKinh;
+export let showInfo = (clickId) => {
+  let contentHTML = "";
+  //lấy src url đưa vào contentHTML
+  contentHTML = `<p>Brand: ${dataGlasses[clickId].brand} - Name: ${dataGlasses[clickId].name}</p></br>
+  <p>Price: ${dataGlasses[clickId].price} $</p></br>
+  <p>Description:${dataGlasses[clickId].description}</p></br>`;
+  console.log("contentHTML virtual: ", contentHTML);
+  //show contentHTML
+  document.getElementById("glassesInfo").innerHTML = contentHTML;
+  document.getElementById("glassesInfo").style.display = "block";
+};
+window.showInfo = showInfo;
+
